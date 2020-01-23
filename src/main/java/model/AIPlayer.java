3d1067e8f5287467e3e98e83fbd3bb9be33d2b88
buckovate <buckovate@gmail.com>
2020-01-23 16:34:26 +0000

    Create AIPlayer.java
    AI Player class

---
+++ src/main/java/model/AIPlayer.java
@@ -0,0 +1,18 @@
+package model;
+
+public class AIPlayer extends Player {
+
+    public AIPlayer(String name) {
+        super(name);
+    }
+
+    public void chooseAttribute() {
+    //TODO: go through all categories in players top card and find the highest attribute
+    }
+
+
+
+
+
+
+}
