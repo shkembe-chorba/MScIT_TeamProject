ac3b6442cf69c55cbe2e152116f1eb288c89693a
buckovate <buckovate@gmail.com>
2020-01-24 15:49:47 +0000

    Update PlayerTest.java
    test of player class

--- src/test/java/model/PlayerTest.java
+++ src/test/java/model/PlayerTest.java
@@ -1,55 +1,25 @@
 package model;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-/**
- * Example test - Note: Not confirmed for production 
- * TODO: Update
- */
 public class PlayerTest {
 
-    @Nested
-    @DisplayName("Unsupported Names")
-    public class UnsupportedNames {
-
-        final public String emptyName = "";
-        final public String longName = "ThisIsSuchACrazyNameIAmSureItWillNotWork";
-        private Player testPlayer;
-
-        @BeforeEach
-        public void init() {
-            testPlayer = new Player("Gonzo");
-        }
+        Player testPlayer = new Player("USER");
 
         @Test
-        @DisplayName("A player must have a name which isn't an empty string.")
-        public void playerIsPassedAnEmptyString() {
-
-            assertThrows(IllegalArgumentException.class, () -> {
-                new Player(emptyName);
-            }, "Constructor fails");
-
-            assertThrows(IllegalArgumentException.class, () -> {
-                testPlayer.setName(emptyName);
-            }, "setName fails");
-        }
-
-        @Test
-        @DisplayName("The name must be below 32 characters for database storage")
-        public void nameLengthGreaterThan32() {
-
-            assertThrows(IllegalArgumentException.class, () -> {
-                new Player(longName);
-            }, "Constructor fails");
-
-            assertThrows(IllegalArgumentException.class, () -> {
-                testPlayer.setName(longName);
-            }, "setName fails");
-        }
-
+public void testGetName() {
+        String expectedString = "USER";
+        String actualString = testPlayer.toString();
+        assertEquals(expectedString, actualString);
     }
+
+    @Test
+public void testIncrementingRounds() {
+        testPlayer.wonRound();
+        int expectedNumber = 1;
+        int actualNumber = testPlayer.getRoundsWon();
+        assertEquals(expectedNumber, actualNumber);
+}
+
 }
