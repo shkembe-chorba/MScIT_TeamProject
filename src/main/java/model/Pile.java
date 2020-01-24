0a331903c08d124210b1cf24c646fe5d5569c671
buckovate <buckovate@gmail.com>
2020-01-24 16:00:28 +0000

    Merge branch 'feature/gamemodel' of C:\Users\Terezka\Documents\GitHub\Project-CHorba\MScIT_TeamProject with conflicts.

---
+++ src/main/java/model/Pile.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Pile {
+    private Card topCard;
+    public Pile(){
+        this.topCard = topCard;
+    }
+
+    public Card peek() {
+        return topCard;
+    }
+
+    public void shuffle() {};
+    public void split() {};
+}
---
+++ src/main/java/model/RoundInformation.java
@@ -0,0 +1,8 @@
+package model;
+
+public class RoundInformation {
+    private Player winningPlayer;
+    private Card winningCard;
+
+
+}
