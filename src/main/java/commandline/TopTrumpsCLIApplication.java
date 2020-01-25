b1192f35385768e1a897dd6359be2d3c7dcb7ccf
Gareth Sears <garethsears@gmail.com>
2020-01-25 14:04:18 +0000

    amend TopTrumpsCliApplication for MVC (note: quit deferred to within TopTrumpsController)

--- src/main/java/commandline/TopTrumpsCLIApplication.java
+++ src/main/java/commandline/TopTrumpsCLIApplication.java
@@ -8,26 +8,32 @@ public class TopTrumpsCLIApplication {
 	/**
 	 * This main method is called by TopTrumps.java when the user specifies that they want to run in
 	 * command line mode. The contents of args[0] is whether we should write game logs to a file.
- 	 * @param args
+	 * 
+	 * @param args
 	 */
 	public static void main(String[] args) {
 
 		boolean writeGameLogsToFile = false; // Should we write game logs to file?
-		if (args[0].equalsIgnoreCase("true")) writeGameLogsToFile=true; // Command line selection
-		
-		// State
-		boolean userWantsToQuit = false; // flag to check whether the user wants to quit the application
-		
-		// Loop until the user wants to exit the game
-		while (!userWantsToQuit) {
+		if (args[0].equalsIgnoreCase("true"))
+			writeGameLogsToFile = true; // Command line selection
 
-			// ----------------------------------------------------
-			// Add your game logic here based on the requirements
-			// ----------------------------------------------------
-			
-			userWantsToQuit=true; // use this when the user wants to exit the game
-			
-		}
+		TopTrumpsModel model = new TopTrumpsModel();
+		TopTrumpsController controller = new TopTrumpsController(model, writeGameLogsToFile);
+		TopTrumpsView view = new TopTrumpsView(controller);
+		controller.addView(view);
+
+		// THE BELOW LOOP HAS BEEN MOVED TO THE CONTROLLER
+
+		// // This will be moved to the controller.
+		// // // Loop until the user wants to exit the game
+		// while (true) {
+
+		// // ----------------------------------------------------
+		// // Add your game logic here based on the requirements
+		// // ----------------------------------------------------
+
+		// }
+
 
 
 	}
