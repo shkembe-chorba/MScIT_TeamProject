d974a3ae1f93d05c52d1d9e72b774ce703981c1d
Nadezhda Dimitrova <nadya.dimitrova257@gmail.com>
2020-01-26 11:23:13 +0000

    Wrote a test for the queries in the same style as Filip's. Created methods for setup and tearDown in order to avoid code duplication.

--- src/test/java/model/DatabaseTest.java
+++ src/test/java/model/DatabaseTest.java
@@ -1,5 +1,7 @@
 package model;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,26 +15,31 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 class DatabaseTest {
-    /**
-     *
-     * @throws Exception this is done because if we do a try-catch block our test reports a false positive when an exception is caught.
-     * An alternative to the throws exception would be an Assert.fail() in the catch block.
-     */
-    @Test
-    @DisplayName("Check if the game statistics are uploaded correctly to the database.")
-    void uploadGameStats() throws Exception {
 
+    private Connection conn;
+    private Savepoint savepoint;
+
+    private Player user;
+    private Player ai1;
+    private Player ai2;
+    private Player ai3;
+    private Player ai4;
+
+    Player[] players;
+
+    @BeforeEach
+    void setUp() throws Exception {
         //Connect to database.
         Database.connect();
 
         //Get the database connection.
-        Connection conn = Database.getConnection();
+        conn = Database.getConnection();
 
         //Turn off autoCommit for test.
         conn.setAutoCommit(false);
 
         //Set a savepoint to roll back to after test.
-        Savepoint savepoint1 = conn.setSavepoint("Savepoint1");
+        savepoint = conn.setSavepoint("Savepoint1");
 
         //Create statement.
         Statement statement = conn.createStatement();
@@ -47,11 +54,14 @@ class DatabaseTest {
         statement.executeBatch();
 
         //Set up mock players for test.
-        Player user = mock(Player.class);
-        Player ai1 = mock(Player.class);
-        Player ai2 = mock(Player.class);
-        Player ai3 = mock(Player.class);
-        Player ai4 = mock(Player.class);
+        user = mock(Player.class);
+        ai1 = mock(Player.class);
+        ai2 = mock(Player.class);
+        ai3 = mock(Player.class);
+        ai4 = mock(Player.class);
+
+        //Set up player array for test.
+        players = new Player[] {user, ai1, ai2, ai3, ai4};
 
         //Set up names of players.
         when(user.getName()).thenReturn("USER");
@@ -59,9 +69,6 @@ class DatabaseTest {
         when(ai2.getName()).thenReturn("AI2");
         when(ai3.getName()).thenReturn("AI3");
         when(ai4.getName()).thenReturn("AI4");
-
-        //Set up player array for test.
-        Player[] players = {user, ai1, ai2, ai3, ai4};
 
         //Set the roundsWon for the players in the first game for the test.
         when(user.getRoundsWon()).thenReturn(2);
@@ -82,6 +89,26 @@ class DatabaseTest {
 
         //Upload the second game to the database.
         Database.uploadGameStats(3, 15, "USER", players);
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        //Roll back to savepoint1.
+        conn.rollback(savepoint);
+
+        //Disconnect from database.
+        Database.disconnect();
+    }
+
+    /**
+     * @throws Exception this is done because if we do a try-catch block our test reports a false positive when an exception is caught.
+     *                   An alternative to the throws exception would be an Assert.fail() in the catch block.
+     */
+    @Test
+    @DisplayName("Check if the game statistics are uploaded correctly to the database.")
+    void uploadGameStats() throws Exception {
+        //Create statement.
+        Statement statement = conn.createStatement();
 
         //Set up the expected game relation as a String.
         String game_expected = "1 2 20 AI1\n" +
@@ -114,17 +141,39 @@ class DatabaseTest {
                     resultSet.getString("r_won") + "\n";
         }
 
-        //Roll back to savepoint1.
-        conn.rollback(savepoint1);
-
-        //Disconnect from database.
-        Database.disconnect();
-
         //Make Strings for the expected data and the data from the database.
         String data = game + rounds_won;
         String data_expected = game_expected + rounds_won_expected;
 
         //Compare the data from the database to the expected data.
         assertEquals(data_expected, data);
+    }
+
+    /**
+     * @throws Exception this is done because if we do a try-catch block our test reports a false positive when an exception is caught.
+     *                   An alternative to the throws exception would be an Assert.fail() in the catch block.
+     */
+    @Test
+    @DisplayName("Check if the returned game statistics are as expected.")
+    void checkReturnedGameStats() throws Exception {
+        //Set the roundsWon for the players in the second game for the test.
+        when(user.getRoundsWon()).thenReturn(7);
+        when(ai1.getRoundsWon()).thenReturn(1);
+        when(ai2.getRoundsWon()).thenReturn(2);
+        when(ai3.getRoundsWon()).thenReturn(3);
+        when(ai4.getRoundsWon()).thenReturn(0);
+
+        //Upload the third game to the database.
+        Database.uploadGameStats(5, 13, "AI3", players);
+
+        final RetrievedGameStatistics expectedStatistics =
+                new RetrievedGameStatistics(3, 2, 1, 3.333, 20);
+        final RetrievedGameStatistics returnedStatistics = Database.retrieveGameStats();
+
+        assertEquals(expectedStatistics.getAvgDraws(), returnedStatistics.getAvgDraws(), 0.01); // 0.01 is the error margin
+        assertEquals(expectedStatistics.getGamesWonByAi(), returnedStatistics.getGamesWonByAi());
+        assertEquals(expectedStatistics.getGamesWonByUser(), returnedStatistics.getGamesWonByUser());
+        assertEquals(expectedStatistics.getMaxRounds(), returnedStatistics.getMaxRounds());
+        assertEquals(expectedStatistics.getTotalGamesPlayed(), returnedStatistics.getTotalGamesPlayed());
     }
 }
