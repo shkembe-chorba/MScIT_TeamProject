3566d2cf8b30739cc9fb34d6eab0adb4f3f6ec54
buckovate <buckovate@gmail.com>
2020-01-26 09:44:12 +0000

    Create AIPlayerTest.java

---
+++ src/test/java/model/AIPlayerTest.java
@@ -0,0 +1,18 @@
+package model;
+
+package model;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AIPlayerTest {
+    Card topCardValues= {1, 2, 3, 4, 100 };
+    AIPlayer ai = new AIPlayer("AI1");
+
+
+    @Test
+    public class tryAttitude() {
+
+        int expected = ai.chooseAttribute();
+        assertEquals(100, expected );
+    }
+}
