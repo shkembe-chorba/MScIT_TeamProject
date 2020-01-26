f83767899924ecd09a50c996e0c51685732a9a9a
buckovate <buckovate@gmail.com>
2020-01-26 09:44:08 +0000

    Create AIPlayer.java

---
+++ src/main/java/model/AIPlayer.java
@@ -0,0 +1,21 @@
+package model;
+
+public class AIPlayer extends Player {
+
+    public AIPlayer(String name) {
+        super(name);
+    }
+
+    public void chooseAttribute() {
+        Card topCardValues [] = cardValues(peekCard);
+        int max = 0;
+        int i = 0;
+
+        for (int i = 0; i < 5; i++) {
+            if (max < values[i]) {
+                max = values[i]; }
+        }
+        return i;
+    }
+
+}
