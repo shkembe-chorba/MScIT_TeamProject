fda81a92d6941c8b17e39410d756870829e48111
buckovate <buckovate@gmail.com>
2020-01-27 16:28:05 +0000

    Update AIPlayerTest.java

--- src/test/java/model/AIPlayerTest.java
+++ src/test/java/model/AIPlayerTest.java
@@ -8,7 +8,13 @@ public class AIPlayerTest {
     Card topCardValues= {1, 2, 3, 4, 100 };
     AIPlayer ai = new AIPlayer("AI1");
 
+    @Nested
+    @Test
+    public class chooseRightAttribute() {
 
+        int expected = ai.chooseAttribute();
+        assertEquals(4, expected );
+    }
     @Test
     public class chooseRightAttribute() {
 
