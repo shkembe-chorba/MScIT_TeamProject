610c8b1bd50b46b8a999d1b4360222fb78e96047
buckovate <44024006+buckovate@users.noreply.github.com>
2020-01-28 17:23:09 +0000

    Update AIPlayerTest.java

--- src/test/java/model/AIPlayerTest.java
+++ src/test/java/model/AIPlayerTest.java
@@ -8,17 +8,10 @@ public class AIPlayerTest {
     Card topCardValues= {1, 2, 3, 4, 100 };
     AIPlayer ai = new AIPlayer("AI1");
 
-    @Nested
     @Test
     public class chooseRightAttribute() {
 
-        int expected = ai.chooseAttribute();
-        assertEquals(4, expected );
-    }
-    @Test
-    public class chooseRightAttribute() {
-
-        int expected = ai.chooseAttribute();
-        assertEquals(4, expected );
+        int indexActual = ai.chooseAttribute();
+        assertEquals(4, actual);
     }
 }
