4bb8027c857f404b4ae73ba5acb7f88b98ebe2ad
Gareth Sears <garethsears@gmail.com>
2020-01-28 12:55:13 +0000

    setup logger in main

--- src/main/java/commandline/TopTrumpsCLIApplication.java
+++ src/main/java/commandline/TopTrumpsCLIApplication.java
@@ -1,36 +1,20 @@
 package commandline;
 
 import java.io.IOException;
-import java.util.logging.FileHandler;
-import java.util.logging.Formatter;
-import java.util.logging.Handler;
-import java.util.logging.LogManager;
-import java.util.logging.LogRecord;
-import java.util.logging.Logger;
-import model.Player;
+import commandline.utils.Logger;
 
 /**
  * Top Trumps command line application
  */
 public class TopTrumpsCLIApplication {
 
-	private static final String LOGGER_FILENAME = "toptrumps.log";
 	private static final String LOGGER_DIRECTORY = "./";
-
-	private static final int LOGGER_IO_ERROR_CODE = 1;
-	private static final String LOGGER_IO_ERROR =
-			"Sorry, there is a problem writing your log file to disk. "
-					+ "Try again later or run without logging.";
-
-	private static final int LOGGER_SECURITY_ERROR_CODE = 2;
+	private static final String LOGGER_FILENAME = "TopTrumps.log";
 	private static final String LOGGER_SECURITY_ERROR =
-			"Sorry, the logger has created a security violation. "
-					+ "Try again later or run without logging.";
-
-	private static final String LOGGER_DIVIDER_STRING = "--------";
-
-	private static final Logger LOGGER = Logger.getLogger("commandline");
-	private static Handler loggerHandler;
+			"There is a security error with the logger.";
+	private static final int LOGGER_SECURITY_ERROR_CODE = 1;
+	private static final String LOGGER_IO_ERROR = "There is a file system error with the logger.";
+	private static final int LOGGER_IO_ERROR_CODE = 1;
 
 	/**
 	 * This main method is called by TopTrumps.java when the user specifies that they want to run in
@@ -46,18 +30,22 @@ public class TopTrumpsCLIApplication {
 			writeGameLogsToFile = true; // Command line selection
 		}
 
+		// Setup Logger
+
+		Logger logger = new Logger(LOGGER_DIRECTORY + LOGGER_FILENAME);
+
 		if (writeGameLogsToFile) {
-
-		}
-
-		try {
-			setupLogger();
-		} catch (SecurityException e) { // Handle security errors gracefully
-			System.out.println(LOGGER_SECURITY_ERROR);
-			System.exit(LOGGER_SECURITY_ERROR_CODE);
-		} catch (IOException e) { // Handle io errors gracefully
-			System.out.println(LOGGER_IO_ERROR);
-			System.exit(LOGGER_IO_ERROR_CODE);
+			try {
+				logger.enable();
+			} catch (SecurityException e) { // Exit and Handle security errors gracefully
+				displayLoggerError(LOGGER_SECURITY_ERROR);
+				System.exit(LOGGER_SECURITY_ERROR_CODE);
+			} catch (IOException e) { // Exit and Handle io errors gracefully
+				displayLoggerError(LOGGER_IO_ERROR);
+				System.exit(LOGGER_IO_ERROR_CODE);
+			}
+		} else {
+			logger.disable();
 		}
 
 		// State
@@ -66,31 +54,18 @@ public class TopTrumpsCLIApplication {
 
 		// Loop until the user wants to exit the game
 		while (!userWantsToQuit) {
-			Logger.getLogger("commandline").info("This is a test log message");
+			Logger.log("This is a test log message");
 			// ----------------------------------------------------
 			// Add your game logic here based on the requirements
 			// ----------------------------------------------------
-			new Player("Gareth");
 			userWantsToQuit = true; // use this when the user wants to exit the game
-
+			Logger.log("This is another one");
 		}
 	}
 
-	private static void setupLogger() throws SecurityException, IOException {
-		// Remove default logging output to System.err
-		LogManager.getLogManager().reset();
-		// Create a Logger object in main scope (so it isn't garbage collected).
-		loggerHandler = new FileHandler(LOGGER_DIRECTORY + LOGGER_FILENAME);
-		// The default format for messages is to append LOGGER_DIVIDER_STRING
-		loggerHandler.setFormatter(new Formatter() {
-			@Override
-			public String format(LogRecord record) {
-				return String.format("%s\n%s\n", record.getMessage(), LOGGER_DIVIDER_STRING);
-			}
-		});
-
-		LOGGER.addHandler(loggerHandler);
+	private static void displayLoggerError(String message) {
+		System.out.println(message);
+		System.out.println("Please try again later or run without logging.");
 	}
-
 
 }
