973ac6ef4d03a47367088018762caf19b1a85c27
Ventsislav Antov <2504299a@student.gla.ac.uk>
2020-01-29 17:08:47 +0000

    changed json reference

--- src/test/java/model/PileTest.java
+++ src/test/java/model/PileTest.java
@@ -4,14 +4,14 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class PileTest {
+class PileTest {
 
-    Card c0 = new Card("1");
-    Card c1 = new Card("2");
-    Card c2 = new Card("3");
-    Card c3 = new Card("4");
+    static Card c0 = new Card("1");
+    static Card c1 = new Card("2");
+    static Card c2 = new Card("3");
+    static Card c3 = new Card("4");
 
-    void addCardHelper(Pile pile) {
+    static void addCardHelper(Pile pile) {
         pile.add(c0);
         pile.add(c1);
         pile.add(c2);
@@ -23,6 +23,24 @@ public class PileTest {
     void initialiseWithEmptyPile() {
         Pile pile = new Pile();
         assertEquals(0, pile.size());
+    }
+
+    @DisplayName("Shows correct size and tests the add method")
+    @Test
+    void sizeAndAdd(){
+        Pile pile = new Pile();
+        addCardHelper(pile);
+        assertEquals(4, pile.size());
+    }
+
+    //No need to test shuffle method as this is built in
+
+    @DisplayName("Shows correct card when peeked")
+    @Test
+    void peek(){
+        Pile pile = new Pile();
+        PileTest.addCardHelper(pile);
+        assertEquals(c0, pile.peek());
     }
 
     @DisplayName("Can peek the top card without removing it")
@@ -38,6 +56,9 @@ public class PileTest {
     @DisplayName("Pop removes the top card")
     @Test
     void popRemovesTopCardAndReturns() {
-
+        Pile pile = new Pile();
+        addCardHelper(pile);
+        pile.pop();
+        assertEquals(3, pile.size());
     }
 }
