6d7bfd6bfa9c4c8cc6019820f96c3219b744555d
Gareth Sears <garethsears@gmail.com>
2020-01-29 10:06:01 +0000

    add constructor tests to AttributeTest

--- src/test/java/model/AttributeTest.java
+++ src/test/java/model/AttributeTest.java
@@ -3,8 +3,28 @@ package model;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 
 public class AttributeTest {
+
+    @Nested
+    private class Constructor {
+        @DisplayName("Constructor does not accept empty attribute names")
+        @Test
+        public void doesNotAcceptEmptyAttributeNames() {
+            assertThrows(IllegalArgumentException.class, () -> new Attribute("", 1));
+        }
+
+        @DisplayName("Does not accept attribute values less than 0")
+        @Test
+        public void doesNotAcceptValuesLessThanZero() {
+            assertThrows(IllegalArgumentException.class, () -> new Attribute("Test", -1));
+        }
+    }
+
+
     @Test
     public void testGetAttributeName() {
         Attribute testAttribute = new Attribute("Strength", 1);
@@ -38,4 +58,12 @@ public class AttributeTest {
         int actualInt = testAttribute.getValue();
         assertEquals(expectedInt, actualInt);
     }
+
+    // @Nested
+    // private class CompareTo {
+    // @DisplayName
+    // @Test
+
+
+    // }
 }
