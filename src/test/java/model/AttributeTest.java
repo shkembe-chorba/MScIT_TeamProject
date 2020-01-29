088e0f0d143364af57c924cc6e550e3863af06d9
Gareth Sears <garethsears@gmail.com>
2020-01-29 10:29:32 +0000

    add compareTo tests for Attribute class

--- src/test/java/model/AttributeTest.java
+++ src/test/java/model/AttributeTest.java
@@ -1,11 +1,14 @@
 package model;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 
 public class AttributeTest {
 
@@ -59,11 +62,33 @@ public class AttributeTest {
         assertEquals(expectedInt, actualInt);
     }
 
-    // @Nested
-    // private class CompareTo {
-    // @DisplayName
-    // @Test
+    @Nested
+    private class CompareTo {
+        @DisplayName("Attribute with higher value is treated as greater")
+        @Test
+        public void attributeWithHigherValueIsGreater() {
+            Attribute a1 = new Attribute("Strength", 10);
+            Attribute a2 = new Attribute("Stamina", 4);
+            Attribute a3 = new Attribute("Strength", 10);
+            Attribute a4 = new Attribute("Strength", 12);
 
+            List<Attribute> list = Arrays.asList(new Attribute[] {a1, a2, a3, a4});
 
-    // }
+            assertTrue(a1.compareTo(a2) > 0);
+            assertFalse(a2.compareTo(a1) < 0);
+            assertTrue(a1.compareTo(a3) == 0);
+            assertEquals(a4, Collections.max(list));
+        }
+
+        @DisplayName("Attributes with different names cannot be compared")
+        @Test
+        public void attributesWithDifferentNamesCannotBeCompared() {
+
+            Attribute a1 = new Attribute("Strength", 10);
+            Attribute a2 = new Attribute("Stamina", 4);
+
+            assertThrows(IllegalArgumentException.class, () -> a1.compareTo(a2));
+        }
+
+    }
 }
