24afe1a27c410ebab2b89cfa752aa6d14b644f44
Gareth Sears <garethsears@gmail.com>
2020-01-29 12:41:20 +0000

    init test skeleton for CardTest

--- src/test/java/model/CardTest.java
+++ src/test/java/model/CardTest.java
@@ -1,51 +1,72 @@
 package model;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class CardTest {
-    @Test
-    public void testCardValue() {
-    Card testCard = new Card("Spaceship");
-    testCard.att[0] .setValue(1);
-    int expectedInt = 1;
-    int actualInt = testCard.getValue(0);
-    assertEquals(expectedInt, actualInt);
-}
 
-    @Test
-    public void testCardAttribute() {
-        Card testCard = new Card("Spaceship");
-        testCard.att[0].setName("Spade");
-        String expectedString = "Spade";
-        String actualString = testCard.getAttribute(0);
-        assertEquals(expectedString, actualString);
+    private static final Attribute a0 = new Attribute("Strength", 1);
+    private static final Attribute a1 = new Attribute("Stamina", 11);
+    private static final Attribute a2 = new Attribute("Money", 4);
+
+    private static void addAttributesToCard(Card card) {
+        card.add(a0);
+        card.add(a1);
+        card.add(a2);
     }
 
-   
-    @DisplayName("Tests the ArrayList")
-    @Test
-    public void testGetCategoryValues() {
-        Card testCard = new Card("Spaceship");
-        ArrayList<Integer> a = new ArrayList<Integer>();
-        testCard.att[0].setValue(1);
-        testCard.att[1].setValue(3);
-        testCard.att[2].setValue(5);
-        testCard.att[3].setValue(7);
-        testCard.att[4].setValue(2);
-        a.add(1);
-        a.add(3);
-        a.add(5);
-        a.add(7);
-        a.add(2);
+    @Nested
+    private class Constructor {
 
-        ArrayList<Integer> expected = a;
-        ArrayList<Integer> actual = testCard.getCategoryValues();
-        assertEquals(expected, actual);
+        @DisplayName("Throws if passed no name")
+        @Test
+        void throwsIfNameEmpty() {
+            assertThrows(IllegalArgumentException.class, () -> {
+                new Card("");
+            });
+        }
+    }
+
+    @Test
+    public void canGetName() {
+
+    }
+
+    @Nested
+    private class Add {
+        @Test
+        public void canAddAttribute() {
+
+        }
+    }
+
+    @Nested
+    private class GetAttribute {
+
+    }
+
+    @Nested
+    private class GetAttributes {
+        @DisplayName("Returns an attribute list")
+        @Test
+        void getsAttributeList() {
+
+        }
+    }
+
+
+
+    @Test
+    public void canGetAttributeByIndex() {
+        Card testCard = new Card("Spaceship");
+        addAttributesToCard(testCard);
+        assertEquals(a0, testCard.getAttribute(0));
+        assertEquals(a2, testCard.getAttribute(2));
     }
 
 }
