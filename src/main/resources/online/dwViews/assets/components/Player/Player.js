97ec741fbfc0b5ef307bf294dfc6ff8136cec7c2
Gareth Sears <garethsears@gmail.com>
2020-02-08 15:11:50 +0000

    refactor player to follow standard component structure

--- src/main/resources/online/dwViews/assets/components/Player/Player.js
+++ src/main/resources/online/dwViews/assets/components/Player/Player.js
@@ -1,109 +1,68 @@
-const TEST_JSON = {
-  players: [
-    {
-      name: "USER",
-      isAI: false,
-      isActive: true,
-      deckSize: 5,
-      card: {
-        name: "Card",
-        attributes: [
-          {
-            name: "test attribute",
-            value: 12,
-          },
-          {
-            name: "test attribute 2",
-            value: 5,
-          },
-        ],
-      },
-    },
-    {
-      name: "AI1",
-      isAI: true,
-      isActive: false,
-      deckSize: 15,
-      card: {
-        name: "AI Card",
-        attributes: [
-          {
-            name: "test attribute",
-            value: 4,
-          },
-          {
-            name: "test attribute 2",
-            value: 20,
-          },
-        ],
-      },
-    },
-  ],
-};
-
-const attributeTemplate = (attribute) => {
-  return `
-      <li class="list-group-item d-flex justify-content-between align-items-center">
-        ${attribute.name}
-        <span class="badge badge-primary badge-pill">${attribute.value}</span>
-      </li>
-    `;
-};
-
-const cardTemplate = (card) => {
-  return `
-    <div class="card-body">
-
-        <div class="card-hider">
-            <div class="row align-items-center" style="height: 100%">
-                <div class="col text-center">
-                    <h3>Top Trumps</h3>
-                </div>
-            </div>
-        </div>
-
-        <h4 class="card-title">${card.name}</h4>
-
-        <ul class="list-group list-group-flush">
-            ${card.attributes.map((a) => attributeTemplate(a))}
-        </ul>
-    </div>
-    `;
-};
-
-const playerTemplate = (player) => {
-  const icon = player.isAI
-    ? `<i class="fa fa-desktop" aria-hidden="true"></i>`
-    : `<i class="fa fa-user" aria-hidden="true"></i>`;
-
-  return `
-    <div class="card">
-      <div class="card-header">
-        <div class="row ">
-            <div class="col-12 d-flex justify-content-center">
-                <h3>
-                    <span class="tt-is-active badge">
-                        ${icon}
-                        ${player.name}
-                        <i class="fa fa-star" aria-hidden="true"></i>
-                    </span>
-                </h3>
-            </div>
-            <div class="col-12 d-flex justify-content-center">
-                <span class="badge badge-light">
-                    Cards in deck: <span class="badge badge-primary badge-pill">${
-                      player.deckSize
-                    }</span>
-                </span>
-            </div>
-        </div>
-    </div>
-    ${cardTemplate(player.topCard)}
-  </div>
-    `;
-};
-
 const PlayerFactory = (playerObj) => {
+  // TEMPLATES
+  // Stored inside the function to prevent conflicts.
+  const attributeTemplate = (attribute) => {
+    return `
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+          ${attribute.name}
+          <span class="badge badge-primary badge-pill">${attribute.value}</span>
+        </li>
+      `;
+  };
+
+  const cardTemplate = (card) => {
+    return `
+      <div class="card-body">
+
+          <div class="card-hider">
+              <div class="row align-items-center" style="height: 100%">
+                  <div class="col text-center">
+                      <h3>Top Trumps</h3>
+                  </div>
+              </div>
+          </div>
+
+          <h4 class="card-title">${card.name}</h4>
+
+          <ul class="list-group list-group-flush">
+              ${card.attributes.map((a) => attributeTemplate(a))}
+          </ul>
+      </div>
+      `;
+  };
+
+  const playerTemplate = (player) => {
+    const icon = player.isAI
+      ? `<i class="fa fa-desktop" aria-hidden="true"></i>`
+      : `<i class="fa fa-user" aria-hidden="true"></i>`;
+
+    return `
+      <div class="card">
+        <div class="card-header">
+          <div class="row ">
+              <div class="col-12 d-flex justify-content-center">
+                  <h3>
+                      <span class="tt-is-active badge">
+                          ${icon}
+                          ${player.name}
+                          <i class="fa fa-star" aria-hidden="true"></i>
+                      </span>
+                  </h3>
+              </div>
+              <div class="col-12 d-flex justify-content-center">
+                  <span class="badge badge-light">
+                      Cards in deck: <span class="badge badge-primary badge-pill">${
+                        player.deckSize
+                      }</span>
+                  </span>
+              </div>
+          </div>
+      </div>
+      ${cardTemplate(player.topCard)}
+    </div>
+      `;
+  };
+
   // Store an internal reference to the overall player object via passed in variable
 
   // PRIVATE VARIABLES / CONSTRUCTOR (Uses a closure):
