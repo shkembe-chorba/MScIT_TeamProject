17089ed3006c66be6b04b57544f92c5d5b8e5ded
Gareth Sears <garethsears@gmail.com>
2020-02-08 15:49:37 +0000

    bug fixes and optimisations

--- src/main/resources/online/dwViews/assets/components/PlayButton/PlayButton.js
+++ src/main/resources/online/dwViews/assets/components/PlayButton/PlayButton.js
@@ -1,51 +1,46 @@
-const PlayerButtonFactory = (attributes) => {
-  // TEMPLATES
-  // Stored internally in this function to prevent conflicts with other templates, e.g. attributeTemplate.
+const PlayButtonFactory = (attributes) => {
+  // CLASS NAMES
+  // -----------
 
-  // Attribute template
-  const attributeTemplate = (attributeName) => {
-    return `<a id="tt-attribute-selector" class="dropdown-item">${attributeName}</a>`;
-  };
+  const ROUND_BUTTON = "tt-round-button";
+  const DROPDOWN_BUTTON = "tt-dropdown-button";
+  const ATTRIBUTE = "tt-attribute-selector";
+  const ATTRIBUTE_LIST = "tt-attribute-list";
+
+  // TEMPLATES
+  // ---------
 
   // Wrap in a div so our Jquery object only references one thing!
   const buttonTemplate = (attributes) => `
       <div>
-          <button type="button" class="tt-round-button btn btn-primary"> Play round </button>
-          <button type="button" class="tt-dropdown-button btn btn-primary dropdown-toggle"  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" aria-hidden="true">
+          <button type="button" class="${ROUND_BUTTON} btn btn-primary"> Play round </button>
+          <button type="button" class="${DROPDOWN_BUTTON} btn btn-primary dropdown-toggle"  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" aria-hidden="true">
               Play round
           </button>
-          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              ${attributes.map((a) => attributeTemplate(a.name)).join("")}
+          <div class="${ATTRIBUTE_LIST} dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <!-- Attributes inserted here -->
           </div>
       </div>
       `;
 
-  // PRIVATE VARIABLES / CONSTRUCTOR FUNCTIONS
+  const attributeTemplate = (attributeName) => {
+    return `<a class="${ATTRIBUTE} dropdown-item">${attributeName}</a>`;
+  };
+
+  // CONSTRUCTOR / PRIVATE VARIABLES
+  // -------------------------------
 
   // Create jquery reference referencing the button template.
   const $this = $(buttonTemplate(attributes));
 
   // It's probably better to search locally for classes inside THIS's div, as IDs may create conflicts in the
   // long term on the page in general.
-  const $roundButton = $this.find(".tt-round-button");
-  const $dropdownButton = $this.find(".tt-dropdown-button");
-  const $attributes = $this.find(".tt-attribute-selector");
-
-  // Setup an on-click for each of our attributes
+  const $roundButton = $this.find("." + ROUND_BUTTON);
+  const $dropdownButton = $this.find("." + DROPDOWN_BUTTON);
+  const $attributeList = $this.find("." + ATTRIBUTE_LIST);
 
   // This will store the function the user wants to be calledback on a click of an attribute.
   let userCallback = (attributeName) => {};
-
-  // For each Jquery attribute
-  $attributes.each(function() {
-    // Get the jquery object for the attribute
-    const $attribute = this;
-    // When the user clicks this attribute, it calls the 'userCallback' set in the public method below.
-    // It is passed the contents of the attribute anchor (the attribute name).
-    $attribute.click(() => {
-      userCallback($attribute.text().trim());
-    });
-  });
 
   // Public methods
   return {
@@ -53,6 +48,20 @@ const PlayerButtonFactory = (attributes) => {
     setRoundButton: () => {},
     // Todo
     setAttributeButton: () => {},
+    // Add attribute,
+    addAttribute: (attributeName) => {
+      // Create an attribute dom element
+      const $attribute = $(attributeTemplate(attributeName));
+      // Add a click handler that calls the 'userCallback' function when clicked, passing it the attributeName
+      $attribute.click(() => {
+        userCallback(attributeName);
+      });
+      // Add it to our attribute list
+      $attributeList.append($attribute);
+    },
+    clearAttributes: () => {
+      $attributeList.empty();
+    },
     // Append to the target div id / class / ...
     attach: (target) => {
       $(target).append($this);
