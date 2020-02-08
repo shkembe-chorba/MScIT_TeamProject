b2904a7f59a190bb91bb28314ce95974423bbf35
gjs <garethsears@gmail.com>
2020-02-08 23:03:53 +0000

    Frontend: Implement view logic, refactor components, make display responsive and add comments. Working prototype up to Game Over: See Scores (#42)
    * refactor GameScreen with comments
    
    * refactor PlayButton to include GameOver state and optimise hiding
    
    * refactor the component classes with comments and general tidying

--- src/main/resources/online/dwViews/assets/components/PlayButton/PlayButton.js
+++ src/main/resources/online/dwViews/assets/components/PlayButton/PlayButton.js
@@ -1,9 +1,17 @@
+/**
+ * A psuedo class that uses closures to encapsulate information and returns an object
+ * with all the public methods.
+ *
+ * Javascript that controls the main PLAY BUTTON, our unique ONE BUTTON DOES ALL!tm interface.
+ */
+
 const PlayButtonFactory = (attributes) => {
   // CLASS NAMES
   // -----------
 
   const NEXT_ROUND_BUTTON = "tt-next-round-button";
   const PLAY_ROUND_BUTTON = "tt-play-round-button";
+  const GAME_OVER_BUTTON = "tt-game-over-button";
   const DROPDOWN_BUTTON = "tt-dropdown-button";
   const ATTRIBUTE = "tt-attribute-selector";
   const ATTRIBUTE_LIST = "tt-attribute-list";
@@ -14,9 +22,10 @@ const PlayButtonFactory = (attributes) => {
   // Wrap in a div so our Jquery object only references one thing!
   const buttonTemplate = () => `
       <div>
-          <button type="button" class="${PLAY_ROUND_BUTTON} btn btn-primary"> Play round </button>
-          <button type="button" class="${NEXT_ROUND_BUTTON} btn btn-warning"> Next round </button>
-          <button type="button" class="${DROPDOWN_BUTTON} btn btn-primary dropdown-toggle"  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" aria-hidden="true">
+          <button type="button" class="${PLAY_ROUND_BUTTON} btn btn-lg btn-primary"> Play round </button>
+          <button type="button" class="${NEXT_ROUND_BUTTON} btn btn-lg btn-warning"> Next round </button>
+          <button type="button" class="${GAME_OVER_BUTTON} btn btn-lg btn-danger"> Get Scores </button>
+          <button type="button" class="${DROPDOWN_BUTTON} btn btn-lg btn-primary dropdown-toggle"  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" aria-hidden="true">
               Play round
           </button>
           <div class="${ATTRIBUTE_LIST} dropdown-menu" aria-labelledby="dropdownMenuButton">
@@ -29,7 +38,7 @@ const PlayButtonFactory = (attributes) => {
     return `<a class="${ATTRIBUTE} dropdown-item">${attributeName}</a>`;
   };
 
-  // CONSTRUCTOR / PRIVATE VARIABLES
+  // PRIVATE VARIABLES
   // -------------------------------
 
   // Create jquery reference referencing the button template.
@@ -39,58 +48,76 @@ const PlayButtonFactory = (attributes) => {
   // long term on the page in general.
   const $nextRoundButton = $this.find("." + NEXT_ROUND_BUTTON);
   const $playRoundButton = $this.find("." + PLAY_ROUND_BUTTON);
+  const $gameOverButton = $this.find("." + GAME_OVER_BUTTON);
   const $dropdownButton = $this.find("." + DROPDOWN_BUTTON);
+
+  // Store all the buttons in an array for easy hiding
+  const buttonArray = [
+    $nextRoundButton,
+    $playRoundButton,
+    $gameOverButton,
+    $dropdownButton,
+  ];
+
   const $attributeList = $this.find("." + ATTRIBUTE_LIST);
 
   // This will store the function the user wants to be calledback on a click of an attribute.
+  // Initially returns null, is passed the attribute name as a parameter.
   let userCallback = (attributeName) => {
     return null;
   };
 
+  // PRIVATE FUNCTIONS
+
+  // For everything in the button array, hide it.
+  const hideButtons = () => {
+    buttonArray.forEach((b) => b.hide());
+  };
+
+  // CONSTRUCTOR
+
+  // Hide buttons on init.
+  hideButtons();
+
   // Public methods
-  const PlayButton = {
+  return {
     // Show the next round button, hide the others
-
     setNextRoundButton: () => {
-      $playRoundButton.hide();
+      hideButtons();
       $nextRoundButton.show();
-      $dropdownButton.hide();
     },
-
-    // Set the nextround callback function
+    // Set the nextround click callback function
     onNextRoundClick: (callback) => {
       $nextRoundButton.click(callback);
     },
-
     // Show the play round button, hide the others
-
     setPlayRoundButton: () => {
+      hideButtons();
       $playRoundButton.show();
-      $nextRoundButton.hide();
-      $dropdownButton.hide();
     },
-
-
-    // Set the playround callback function
+    // Set the playround click callback function
     onPlayRoundClick: (callback) => {
       $playRoundButton.click(callback);
     },
-
-
+    // Show the game over style button, hide the others
+    setGameOverButton: () => {
+      hideButtons();
+      $gameOverButton.show();
+    },
+    // Set the gameover click callback function
+    onGameOverClick: (callback) => {
+      $gameOverButton.click(callback);
+    },
     // Show the attribute button, hide the others
-
     setAttributeButton: () => {
-      $playRoundButton.hide();
-      $nextRoundButton.hide();
+      hideButtons();
       $dropdownButton.show();
     },
-
     // Sets the user callback function when an attribute is clicked.
-    // The callback should take the attribute name as a parameter.
+    // The callback is passed the attribute name as a parameter.
     onAttributeClick: (callback) => {
       userCallback = callback;
     },
-
     // Add attribute,
     addAttribute: (attributeName) => {
       // Create an attribute dom element
@@ -102,17 +129,13 @@ const PlayButtonFactory = (attributes) => {
       // Add it to our attribute list
       $attributeList.append($attribute);
     },
-
     // Clear all attributes
     clearAttributes: () => {
       $attributeList.empty();
     },
-
     // Append to the target div id / class / ...
     attach: (target) => {
       $(target).append($this);
     },
   };
-
-  return PlayButton;
 };
--- src/main/resources/online/dwViews/assets/components/Player/Player.js
+++ src/main/resources/online/dwViews/assets/components/Player/Player.js
@@ -1,131 +1,168 @@
-// "CLASS" TEMPLATES
-
-const attributeTemplate = (attribute) => {
-  return `<li class="tt-attribute list-group-item d-flex justify-content-between align-items-center">
-        <span class="tt-attribute-name">${attribute.name}</span>
-        <span class="badge badge-primary badge-pill">${attribute.value}</span>
-      </li>`;
-};
-
-const cardTemplate = (card) => {
-  return `
-    <div class="card-body">
-        <div class="card-hider">
-            <div class="row align-items-center" style="height: 100%">
-                <div class="col text-center">
-                    <h3>Top Trumps</h3>
-                </div>
-            </div>
-        </div>
-        <h4 class="card-title">${card.name}</h4>
-        <ul class="list-group list-group-flush">
-            ${card.attributes.map((a) => attributeTemplate(a)).join("")}
-        </ul>
-    </div>
-    `;
-};
-
-const playerTemplate = (player) => {
-  const icon = player.isAI
-    ? `<i class="fa fa-desktop" aria-hidden="true"></i>`
-    : `<i class="fa fa-user" aria-hidden="true"></i>`;
-
-  const iconActive = player.isActive
-    ? `<i class="fa fa-star" aria-hidden="true"></i>`
-    : ``;
-
-  return `
-    <div class="card card-player mt-3">
-      <div class="card-header">
-        <div class="row ">
-            <div class="col-12 d-flex justify-content-center">
-                <h3>
-                    <span class="tt-is-active badge">
-                        ${icon}
-                        ${iconActive}
-                        ${player.name}
-                    </span>
-                    </h3>
-                </div>
-                <div class="col-12 d-flex justify-content-center">
-                    <div class="col text-center">
-                        <span class="tt-deck-size badge badge-light">
-                            Cards in deck:
-                            <span class="badge badge-primary badge-pill">${
-                              player.deckSize
-                            }</span>
-                        </span>
-                    </div>
-                </div>
-             </div>
-          </div>
-            ${cardTemplate(player.topCard)}
-    </div>
-    `;
-};
+/**
+ * A psuedo class that uses closures to encapsulate information and returns an object
+ * with all the public methods.
+ *
+ * @param {Object} playerObj - the player object passed by the api
+ */
 
 const PlayerFactory = (playerObj) => {
-  // Store an internal reference to the overall player object via passed in variable
+  // CLASS NAMES
+  // -----------
+  const NAME_BADGE = "tt-is-active";
+  const ATTRIBUTE_NAME = "tt-attribute-name";
+  const CARD_COVER = "card-hider";
+  const CARD_HEADER = "tt-card-header";
+  const DECK_SIZE = "tt-deck-size";
 
-  // PRIVATE VARIABLES / CONSTRUCTOR (Uses a closure):
-  // -----------------------
+  // TEMPLATES
+  // ---------
+  const attributeTemplate = (attribute) => {
+    return `<li class="list-group-item d-flex justify-content-between align-items-center">
+          <span class="${ATTRIBUTE_NAME}">${attribute.name}</span>
+          <span class="badge badge-primary badge-pill">${attribute.value}</span>
+        </li>`;
+  };
+
+  const cardTemplate = (card) => {
+    return `
+      <div class="card-body">
+          <div class="${CARD_COVER}">
+              <div class="row align-items-center" style="height: 100%">
+                  <div class="col text-center">
+                      <h3>Top Trumps</h3>
+                  </div>
+              </div>
+          </div>
+          <h4 class="card-title">${card.name}</h4>
+          <ul class="list-group list-group-flush">
+              ${card.attributes.map((a) => attributeTemplate(a)).join("")}
+          </ul>
+      </div>
+      `;
+  };
+
+  const playerTemplate = (player) => {
+    const icon = player.isAI
+      ? `<i class="fa fa-desktop" aria-hidden="true"></i>`
+      : `<i class="fa fa-user" aria-hidden="true"></i>`;
+
+    const iconActive = player.isActive
+      ? `<i class="fa fa-star" aria-hidden="true"></i>`
+      : ``;
+
+    return `
+      <div class="card card-player align-self-center mt-3">
+        <div class="${CARD_HEADER} card-header">
+          <div class="row">
+              <div class="col-12 d-flex justify-content-center">
+                  <h3>
+                      <span class="${NAME_BADGE} badge">
+                          ${icon}
+                          ${iconActive}
+                          ${player.name}
+                      </span>
+                      </h3>
+                  </div>
+                  <div class="col-12 d-flex justify-content-center">
+                      <div class="col text-center">
+                          <span class="${DECK_SIZE} badge badge-light">
+                              Cards in deck:
+                              <span class="badge badge-primary badge-pill">${
+                                player.deckSize
+                              }</span>
+                          </span>
+                      </div>
+                  </div>
+               </div>
+            </div>
+              ${cardTemplate(player.topCard)}
+      </div>
+      `;
+  };
+
+  // PRIVATE VARIABLES
+  // -----------------
   // Create a jquery reference inside this 'class' as a private member (using closure)
   const $this = $(playerTemplate(playerObj));
 
-  if (playerObj.isActive) {
-    $this.find(".tt-is-active").addClass("badge-success");
-  } else {
-    $this.find(".tt-is-active").addClass("badge-warning");
-  }
+  const $nameBadge = $this.find("." + NAME_BADGE);
+  const $cardCover = $this.find("." + CARD_COVER);
+  const $cardHeader = $this.find("." + CARD_HEADER);
+  const $deckSize = $this.find("." + DECK_SIZE);
+
+  // PRIVATE METHODS
+  // ---------------
 
   const highlightAttribute = (attributeName, color) => {
     $this
-      .find(`.tt-attribute:has(>.tt-attribute-name:contains(${attributeName}))`)
+      // Find the attribute field that has the attribute name containing the desired name
+      .find(`.${ATTRIBUTE_NAME}:contains(${attributeName})`)
+      .parent() // Get the parent list element and colorize it
       .css("background-color", color)
       .css("color", "white");
   };
 
-  // PUBLIC METHODS:
-  const Player = {
+  // CONSTRUCTOR
+  // -----------
+
+  // If the player is active, change namebadge colour to green, otherwise yellow
+  if (playerObj.isActive) {
+    $nameBadge.addClass("badge-success");
+  } else {
+    $nameBadge.addClass("badge-warning");
+  }
+
+  // PUBLIC METHODS
+  // --------------
+
+  return {
+    // Get the owning player's name
     getName: () => {
       return playerObj.name;
     },
+    // Return if the player is a user
     isUser: () => {
       return playerObj.isAI ? false : true;
     },
-    // Add to a dom element (by ID or class)
+    // Add to a dom element (passed by ID or class as a parameter, e.g. #card-deck)
     attach: (container) => {
       $(container).append($this);
     },
+    // Makes the cardCover visible (see .card-hider in css)
+    // Effectively covers the contents of a card from view
     hideCard: () => {
-      $this.find(".card-hider").addClass("card-hider-hide");
+      $cardCover.addClass("card-hider-hide");
     },
+    // Makes the card attributes visible by removing the css class
     showCard: () => {
-      $this.find(".card-hider").removeClass("card-hider-hide");
+      $cardCover.removeClass("card-hider-hide");
     },
+    // Highlights the namebadge green to show it is a winner.
+    // Also highlights the winning attribute green.
     setWinner: (attributeName) => {
       highlightAttribute(attributeName, "green");
-      $this
-        .find(".tt-is-active")
+      $nameBadge
         .removeClass("badge-danger")
         .removeClass("badge-warning")
         .addClass("badge-success");
     },
+    // Highlights the namebadge red to show it is a loser.
+    // Also highlights the winning attribute red.
     setLoser: (attributeName) => {
       highlightAttribute(attributeName, "red");
-      $this
-        .find(".tt-is-active")
+      $nameBadge
         .removeClass("badge-warning")
         .removeClass("badge-success")
         .addClass("badge-danger");
     },
+    // As above, but a lot more violent. Sets the whole header red
+    // and displays an IS OUT message in the name badge
     eliminate: () => {
       // Set header a violent red
-      $this.find(".card-header").css("background-color", "red");
+      $cardHeader.css("background-color", "red");
 
       // Set label to eliminated
-      $this
-        .find(".tt-is-active")
+      $nameBadge
         .removeClass("badge-warning")
         .removeClass("badge-success")
         .addClass("badge-danger") // Overrides primary and warning
@@ -133,12 +170,7 @@ const PlayerFactory = (playerObj) => {
         .text(`${playerObj.name} IS OUT`);
 
       // Set deck size to red for emphasis on that fat 0.
-      $this
-        .find(".tt-deck-size")
-        .removeClass("badge-light")
-        .addClass("badge-danger");
+      $deckSize.removeClass("badge-light").addClass("badge-danger");
     },
   };
-
-  return Player;
 };
