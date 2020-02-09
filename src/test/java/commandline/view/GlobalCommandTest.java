148906c3903e15b093918e7842285bac0556093e
Gareth Sears <garethsears@gmail.com>
2020-02-09 14:29:39 +0000

    Refactor GlobalCommand tests and get full test coverage

--- src/test/java/commandline/view/GlobalCommandTest.java
+++ src/test/java/commandline/view/GlobalCommandTest.java
@@ -1,19 +1,28 @@
 package commandline.view;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+/**
+ * GlobalCommand tests
+ */
 public class GlobalCommandTest {
 
     @DisplayName("Constructor")
     @Nested
     public class Constructor {
+
+        /*
+         * Defect Tests
+         */
 
         @DisplayName("Throws if not one word")
         @ParameterizedTest
@@ -31,10 +40,15 @@ public class GlobalCommandTest {
         }
     }
 
-    @DisplayName("notifyCommandListeners()")
+    @DisplayName("add/remove/notify GlobalCommandListener")
     @Nested
     public class Listeners {
-        @DisplayName("Listener notified")
+
+        /*
+         * Validation Tests
+         */
+
+        @DisplayName("Listener can be notified")
         @Test
         public void globalCommandInputNotifiesListeners() {
 
@@ -48,11 +62,32 @@ public class GlobalCommandTest {
 
             assertTrue(gcl.triggered);
         }
+
+        @DisplayName("Listener can be removed and is not notified")
+        @Test
+        public void canRemoveGlobalCommandListeners() {
+
+            GlobalCommandListenerTest gcl = new GlobalCommandListenerTest();
+            GlobalCommand gc = new GlobalCommand("Test");
+
+            assertFalse(gcl.triggered);
+
+            gc.addCommandListener(gcl);
+            gc.removeCommandListener(gcl);
+            gc.notifyCommandListeners();
+
+            assertFalse(gcl.triggered);
+        }
     }
 
     @DisplayName("equals()")
     @Nested
     public class Equals {
+
+        /*
+         * Validation Tests
+         */
+
         @DisplayName("Equals is true for same command")
         @Test
         public void equalsWhenCommandIsSame() {
@@ -69,6 +104,25 @@ public class GlobalCommandTest {
             GlobalCommand gc2 = new GlobalCommand("Test2", "description equal");
 
             assertFalse(gc1.equals(gc2));
+        }
+    }
+
+    @DisplayName("toString()")
+    @Nested
+    public class ToString {
+
+        /*
+         * Validation Tests
+         */
+
+        @DisplayName("Displays expected toString() format")
+        @Test
+        public void toStringAsExpected() {
+            GlobalCommand gc1 = new GlobalCommand("Test");
+            GlobalCommand gc2 = new GlobalCommand("Test", "But with a description");
+
+            assertEquals("Test", gc1.toString());
+            assertEquals("Test - But with a description", gc2.toString());
         }
     }
 }
