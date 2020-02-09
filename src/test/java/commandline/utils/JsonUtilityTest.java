53a40b6efe1d9f091a772d746e0458592d8e7d97
Gareth Sears <garethsears@gmail.com>
2020-02-09 13:47:32 +0000

    refactor JsonUtility and Tests

--- src/test/java/commandline/utils/JsonUtilityTest.java
+++ src/test/java/commandline/utils/JsonUtilityTest.java
@@ -1,44 +1,84 @@
 package commandline.utils;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.io.File;
 import java.io.IOException;
+import java.util.UUID;
 import com.google.gson.JsonObject;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * JsonUtility Tests
+ */
+@DisplayName("JsonUtility")
 public class JsonUtilityTest {
 
+    private static final String TEST_RESOURCE_DIRECTORY = "src/test/resources/commandline/utils/";
+
+    private static final String TEST_JSON_FILE = "test.json";
+    private static final String TEST_TEXT_FILE = "test.txt";
 
     @DisplayName("getJsonObjectFromFile()")
     @Nested
     class GetJsonObjectFromFile {
 
-        private static final String TEST_RESOURCE_DIRECTORY =
-                "/src/test/resources/commandline/utils/";
-        private static final String TEST_RESOURCE_FILE = "test.json";
+        /*
+         * Validation Tests
+         */
 
         @DisplayName("Take a file path object and return a JsonObject")
         @Test
         void returnsJsonObject() {
 
-            // Load test file
-            File path = new File(TEST_RESOURCE_DIRECTORY, TEST_RESOURCE_FILE);
+            File testFilePath = new File(TEST_RESOURCE_DIRECTORY, TEST_JSON_FILE);
 
             JsonObject json = null;
-
             try {
-                json = JsonUtility.getJsonObjectFromFile(path);
+                json = JsonUtility.getJsonObjectFromFile(testFilePath);
             } catch (IOException e) {
                 e.printStackTrace();
             }
 
             assertTrue(json.get("test").getAsBoolean());
+        }
 
+        /*
+         * Defect Tests
+         */
+
+        @DisplayName("Throw if file does not exist")
+        @Test
+        void throwIfFileDoesNotExist() {
+            // Generate a garbage test file
+            File nonExistentFile = new File(UUID.randomUUID().toString());
+
+            assertThrows(IllegalArgumentException.class, () -> {
+                try {
+                    JsonUtility.getJsonObjectFromFile(nonExistentFile);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+
+        @DisplayName("Throw if file is not json")
+        @Test
+        void throwIfFileIsNotJson() {
+            // Pass a .txt file
+            File testFile = new File(TEST_RESOURCE_DIRECTORY, TEST_TEXT_FILE);
+
+            assertThrows(IllegalArgumentException.class, () -> {
+                try {
+                    JsonUtility.getJsonObjectFromFile(testFile);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
         }
     }
-
-
-
 }
