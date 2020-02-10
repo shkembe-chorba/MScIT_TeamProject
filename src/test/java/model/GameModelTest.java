556d4cea22ad26e80f508e412ba5b57f558eaea0
Gareth Sears <garethsears@gmail.com>
2020-02-10 15:45:12 +0000

    setup class for GameModel tests

---
+++ src/test/java/model/GameModelTest.java
@@ -0,0 +1,16 @@
+package model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Pile tests
+ */
+class PileTest {
+
+
+
+}
